a37cc16eac09324e221d7165a4745658bf96979d
kyuuki <kyuuki.japan@gmail.com>
2022-09-27 10:59:03 +0000

    本番用の puma 設定追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,10 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  else
+    logger           = ActiveSupport::Logger.new('log/production.log', 5, 1.megabytes)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
