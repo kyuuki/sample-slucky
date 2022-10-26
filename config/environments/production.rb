640b03f136f874a1c86b1906faef6c0979522145
kyuuki <kyuuki.japan@gmail.com>
2022-10-26 04:57:29 +0000

    本番用の設定値を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: Rails.configuration.setting[:host], protocol: "https" }
+  #config.action_mailer.default_url_options = { host: Rails.configuration.setting[:host], protocol: "https" }
+  config.action_mailer.default_url_options = { host: Rails.configuration.setting[:host] }
 
   # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
